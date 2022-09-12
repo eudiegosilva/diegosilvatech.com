55d308bea2331ac4ac5057309e16b95d8fc559aa
Diego Silva <social.diegosilvatech@gmail.com>
2022-09-12 00:48:53 -0300

    feat: update icons

--- src/components/command/icons.ts
+++ src/components/command/icons.ts
@@ -1,15 +1,13 @@
-import aboutIcon from 'assets/static/icons/about.json';
-import articlesIcon from 'assets/static/icons/articles.json';
-import copyLinkIcon from 'assets/static/icons/copy-link.json';
-import emailIcon from 'assets/static/icons/email.json';
+import articlesIcon from 'assets/static/icons/article.json';
+import aboutIcon from 'assets/static/icons/avatar.json';
+import projectsIcon from 'assets/static/icons/bulp.json';
+import talksIcon from 'assets/static/icons/calendar.json';
+import sourceIcon from 'assets/static/icons/code.json';
+import podcastsIcon from 'assets/static/icons/headphones.json';
 import homeIcon from 'assets/static/icons/home.json';
-import investingIcon from 'assets/static/icons/investing.json';
-import podcastsIcon from 'assets/static/icons/podcasts.json';
-import projectsIcon from 'assets/static/icons/projects.json';
-import reminderIcon from 'assets/static/icons/reminder.json';
-import sourceIcon from 'assets/static/icons/source.json';
-import talksIcon from 'assets/static/icons/talks.json';
-import usesIcon from 'assets/static/icons/uses.json';
+import usesIcon from 'assets/static/icons/laptop.json';
+import copyLinkIcon from 'assets/static/icons/link.json';
+import emailIcon from 'assets/static/icons/mail-open.json';
 
 export {
   aboutIcon,
@@ -17,10 +15,8 @@ export {
   copyLinkIcon,
   emailIcon,
   homeIcon,
-  investingIcon,
   podcastsIcon,
   projectsIcon,
-  reminderIcon,
   sourceIcon,
   talksIcon,
   usesIcon
