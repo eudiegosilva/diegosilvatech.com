13505e09989422d2e02e5e7a9191dbfe04c4a8f6
Diego Silva <social.diegosilvatech@gmail.com>
2022-09-18 14:43:13 -0300

    feat: add footer key

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,25 +1,29 @@
-import { links, LinkProps } from './footer-links';
+import { links } from './footer-links';
 
 import * as s from './footer.styles';
 
 export type FooterProps = {};
 
-const renderAnchor = (link: LinkProps) => {
-  if (link.url.startsWith('http')) {
-    return (
-      <s.TextAnchor as="a" key={link.title} href={link.url}>
-        {link.title}
-      </s.TextAnchor>
-    );
-  }
-
-  return <s.TextAnchor as="a">{link.title}</s.TextAnchor>;
-};
-
 const Footer = () => {
   return (
     <s.FooterContainer>
-      <s.FooterWrapper>{links.map(renderAnchor)}</s.FooterWrapper>
+      <s.FooterWrapper>
+        {links.map(link => {
+          if (link.url.startsWith('http')) {
+            return (
+              <s.TextAnchor as="a" key={link.title} href={link.url}>
+                {link.title}
+              </s.TextAnchor>
+            );
+          }
+
+          return (
+            <s.TextAnchor key={link.title} as="a">
+              {link.title}
+            </s.TextAnchor>
+          );
+        })}
+      </s.FooterWrapper>
     </s.FooterContainer>
   );
 };
